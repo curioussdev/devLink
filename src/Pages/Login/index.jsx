09b0f197fa84515c6968c3c69750771accc1ec58
Jose Lopes <946393361bill@gmail.com>
2022-12-04 11:11:51 +0000

    login screen created

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -1,21 +1,36 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
 import './login.css'
 import { Logo } from '../../components/logo'
+
 export default function Login() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleLogin(e){
+        e.preventDefault();
+        console.log(email)
+        console.log(password)
+    }
+
     return(
         <div className='login-container'>
            <Logo/>
 
-           <form className='form'>
+           <form className='form' onSubmit={handleLogin}>
                 <input 
                     type="email"
                     placeholder='Digite seu email'
+                    value={email}
+                    onChange={(e) => {setEmail(e.target.value)}}
                 />
 
                 <input 
                     type="password"
                     placeholder='*********'
                     autoComplete='on'
+                    value={password}
+                    onChange={(e) => {setPassword(e.target.value)}}
                 />
 
                 <button type='submit'>Acessar</button>
