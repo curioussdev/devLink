84be4c5220b1e37223d75f2c356733573494d60e
Jose Lopes <946393361bill@gmail.com>
2022-12-05 01:52:59 +0000

    creating admin screen visuals

--- src/Pages/Admin/index.jsx
+++ src/Pages/Admin/index.jsx
@@ -1,7 +1,63 @@
+import { Header } from "../../components/Header";
+import './admin.css';
+import { Logo } from '../../components/Logo';
+import { Input } from '../../components/Input';
+
+import { MdAddLink } from 'react-icons/md';
+import { FiTrash2 } from 'react-icons/fi'
+
+
 export default function Admin() {
     return(
-        <div>
-            <h1>Página Admin</h1>
+        <div className="admin-container">
+            <Header />
+
+            <Logo />
+
+            <form className="form">
+                <label className="label">Nome do link</label>
+                <Input 
+                    placeholder="Nome do link..."
+                />
+
+                <label className="label">Url do link</label>
+                <Input 
+                    type="url"
+                    placeholder="Nome do link..."
+                />
+
+                <section className="container-colors">
+                    <div>
+                        <label className="label right">Fundo do link</label>
+                        <input type="color" />
+                    </div>
+
+                    <div>
+                        <label className="label right">Cor do link</label>
+                        <input type="color" />
+                    </div>
+                </section>
+
+                <button className="btn-register" type="submit">
+                    Cadastrar Link <MdAddLink size={24} color="#FFF"/>
+                </button>
+
+            </form>
+
+
+            <h2 className="title">Meus Links</h2>
+
+            <article 
+                className="list animate-pop"
+                style={{ backgroundColor: "#000", color: "#fff"}}
+            >
+                <p>Grupo exclusivo no telegram</p>
+                <div>
+                    <button className="btn-delete">
+                        <FiTrash2  size={18} color="#FFF"/>
+                    </button>
+                </div>
+            </article>
         </div>
     )
 }
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+import './header.css'
+import { BiLogOut } from 'react-icons/bi'
+import { Link } from 'react-router-dom'
+
+import { auth } from '../../services/firebaseConnection';
+import { signOut } from 'firebase/auth';
+
+export function Header(){
+    async function handleLogout(){
+        await signOut(auth);
+    }
+
+    return(
+        <header className='admin-header'>
+            <nav className='nav-header'>
+                <button onClick={handleLogout}>
+                    <BiLogOut size={28}  />
+                      
+                </button>
+
+                <Link to="/admin">
+                    Links
+                </Link>
+                <Link to="/admin/social">
+                    Redes Sociais
+                </Link>
+            </nav>
+        </header>
+    )
+}
