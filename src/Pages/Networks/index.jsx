4e5d96718b72b34762f16ff6b48fedcbc983aaaa
Jose Lopes <946393361bill@gmail.com>
2022-12-07 18:31:07 +0000

    styling social networks page

--- src/Pages/Networks/index.jsx
+++ src/Pages/Networks/index.jsx
@@ -1,5 +1,40 @@
+import './networks.css'
+
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input'
+import { MdAddLink } from 'react-icons/md'
+
+
 export default function Networks(){
     return(
-        <h1>REDES SOCIAIS</h1>
+        <div className='admin-container'>
+            <Header />
+
+            <h1 className='title-social'>Suas redes sociais</h1>
+
+            <form className='form' action="">
+                <label className='label'>Link do LinkedIn</label>
+                <Input 
+                    placeholder="Digite a url do LinkedIn..."
+                />
+
+                <label className='label'>Link do Instagram</label>
+                <Input 
+                    placeholder="Digite a url do Instagram..."
+                />
+
+                <label className='label'>Link do Youtube</label>
+                <Input 
+                    placeholder="Digite a url do Youtube..."
+                />
+
+                <button 
+                    type='submit'
+                    className='btn-register'
+                >
+                    Salvar links <MdAddLink size={24} color="#fff" />
+                </button>
+            </form>
+        </div>
     )
 }
