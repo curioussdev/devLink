7fcce9a98f1e248efc401833984c488f78c54d76
Jose Lopes <946393361bill@gmail.com>
2022-12-08 18:42:22 +0000

    fetching data from database

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,9 +1,41 @@
 import './home.css'
+import { useState, useEffect } from 'react'
 
 import { IconsSocial } from '../../components/Icons-Social';
-import { FaInstagram, FaLinkedin, FaYoutube} from 'react-icons/fa'
+import { FaInstagram, FaLinkedin, FaYoutube} from 'react-icons/fa';
+import { db } from '../../services/firebaseConnection';
+import {
+    getDocs,
+    collection,
+    orderBy,
+    query
+} from 'firebase/firestore';
 
 export default function Home(){
+
+    useEffect(()=>{
+        function loadLinks(){
+            const linksRef = collection(db, "links")
+            const queryRef = query(linksRef, orderBy("created_at", "asc"))
+
+            getDocs(queryRef).then((snapshot)=>{
+                let lista = [];
+
+                snapshot.forEach((doc)=>{
+                    lista.push({
+                        id: doc.id,
+                        name: doc.data().name,
+                        url: doc.data().url,
+                        bg: doc.data().bg,
+                        color: doc.data().color
+                    })
+                })
+                console.log(lista)
+            })
+        }
+
+        loadLinks();
+    }, [])
     return(
         <div className='home-container'>
             <h1>Billadas</h1>
@@ -16,24 +48,7 @@ export default function Home(){
                     </a>
                 </section>
 
-                <section className='link-area '>
-                    <a href="#">
-                        <p className='link-text'>Grupo do telegram</p>
-                    </a>
-                </section>
-
-                <section className='link-area '>
-                    <a href="#">
-                        <p className='link-text'>Canal no Youtube</p>
-                    </a>
-                </section>
-
-                <section className='link-area '>
-                    <a href="#">
-                        <p className='link-text'>Instagram</p>
-                    </a>
-                </section>
-
+                
                 <footer>
                     <IconsSocial url="https://www.linkedin.com/in/curioussdev/" >
                         <FaLinkedin size={35} color="#FFF"/>
